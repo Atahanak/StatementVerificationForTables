74ba911c67ba96a5ec8576ca37196baa60ad9940
Atahanak <aakyildiz@sabanciuniv.edu>
2020-12-11 21:19:46 +0300

    tokenizers are working

--- parser.py
+++ parser.py
@@ -15,7 +15,37 @@ def xml2dict(xml_file_name):
     ''' dump xml to dictionary '''
     with open(xml_file_name) as file:
         doc = xmltodict.parse(file.read())
-    return json.dumps(doc, indent=4, sort_keys=True)
+    #return json.dumps(doc, indent=4, sort_keys=True)
+    return doc
+    
+def getLegend(data):
+    return data['document']['table']['legend']['@text']
+
+def getTableElements(table):
+    print("************************************")
+    caption = table['caption']['@text']
+    print("Caption:")
+    print(caption)
+    print()
+
+    print("Rows:")
+    rows = table['row']
+    for r in rows:
+        row =  ""
+        for c in r['cell']:
+            row += c['@text'] + " "
+        print(row)
+
+    legend = table['legend']['@text']
+    print("Legend:")
+    print(legend)
+    
+    statements = table['statements']['statement']
+    print()
+    print("Statements:")
+    for s in statements:
+        print(s['@text'])
+    print("************************************")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -24,4 +54,13 @@ if __name__ == "__main__":
 
     file_name = sys.argv[1]
     data = xml2dict(file_name)
-    print(data)
+    #print(json.dumps(data, indent=4, sort_keys=True))
+    table = data['document']['table']
+    if type(table) == dict:
+        getTableElements(table)        
+    elif type(table) == list:
+        for t in table:
+            getTableElements(t)
+    else:
+        print("Unknown table type.")
+    #print(data)
--- transformer.py
+++ transformer.py
@@ -1,10 +1,26 @@
 ''' transformer example '''
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification 
 
 if __name__ == "__main__":
     print("Learning transformers.")
-    classifier = pipeline('sentiment-analysis')
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+    pt_model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    results = classifier(['lil lol lel', 'this is very pleasing mate.', 'have you ever heard of being pleased', 'i disapprove this type of conversation.'])
-    for result in results:
-        print(f"label: {result['label']}, with score {round(result['score'], 4)}")
+    sac = tokenizer(
+        [("ee alaka", "together"), ("what is this mate ", "this is it our chance to understand tables"), ("kel alaka ", "tuple tuple in the corner")],
+        padding = True,
+        truncation = True,
+        return_tensors="pt"
+    )
+    print(sac)
+
+    import torch
+    pt_outputs = pt_model(**sac)
+    print(pt_outputs)
+
+    
+    import torch.nn.functional as F
+    pt_predictions = F.softmax(pt_outputs[0], dim=-1)
+
+    print(pt_predictions)
