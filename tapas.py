4d17c00364497f55aa726b749f9a449912a6401f
Atahanak <aakyildiz@sabanciuniv.edu>
2021-01-29 11:39:08 +0300

    tapas.py merged with dev branch

--- tapas.py
+++ tapas.py
@@ -17,7 +17,7 @@ def get_tables_from_xml(xml_file_name, tables):
             else:
                 tables.append(TableForVerification(table))
 
-def get_dataset(data_dir):
+def get_dataset(data_dir, tt=None):
     tables = []
     for filename in os.listdir(data_dir):
         if filename.endswith(".xml"): 
@@ -25,14 +25,19 @@ def get_dataset(data_dir):
             get_tables_from_xml(file_path, tables) 
         else:
             continue
-        
+    print("TTT", len(tables)) 
     text_tables = []
     statements = []
     labels = []
     for table in tables:
-        table.populate_tables_statements_labels(text_tables, statements, labels)
-    
-    tokenizer = TapasTokenizer.from_pretrained("google/tapas-base-finetuned-tabfact")
+        if tt == None:
+          table.populate_tables_statements_labels(text_tables, statements, labels)
+        else:
+          table.populate_tables_statements_labels_tt(text_tables, statements, labels)
+          
+    print(labels)
+    print(len(labels)) 
+    tokenizer = TapasTokenizer.from_pretrained("google/tapas-base")
     return TableDataset(text_tables, statements, labels, tokenizer)
 
 train_dir = sys.argv[1]
@@ -41,32 +46,30 @@ test_dir = sys.argv[2]
 from transformers import TapasTokenizer
 from dataset import TableDataset
 #train set
-train_dataset = get_dataset(train_dir) #TableDataset(text_tables, statements, labels, tokenizer)
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4)
+train_dataset = get_dataset(train_dir, True) #TableDataset(text_tables, statements, labels, tokenizer)
 
 #test set
-train_dataset = get_dataset(test_dir) #TableDataset(text_tables, statements, labels, tokenizer)
-test_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4)
+test_dataset = get_dataset(test_dir) #TableDataset(text_tables, statements, labels, tokenizer)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=4)
 
 from transformers import TapasConfig, AdamW, TapasForSequenceClassification
-model = TapasForSequenceClassification.from_pretrained("google/tapas-base-finetuned-tabfact")
+model = TapasForSequenceClassification.from_pretrained("google/tapas-base", num_labels=3)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-optimizer = AdamW(model.parameters(), lr=5e-5) 
+optimizer = AdamW(model.parameters(), lr=3e-5) 
 
 #TRANING
-batch = next(iter(train_dataloader))
-assert batch["input_ids"].shape == (4, 512)
-assert batch["attention_mask"].shape == (4, 512)
-assert batch["token_type_ids"].shape == (4, 512, 7)
-
 from datasets import load_metric
 accuracy = load_metric("accuracy")
-
 print("Starting training...")
 
-epochs = 3
+epochs = 6
 for ep in range(0, epochs):
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4)
+    batch = next(iter(train_dataloader))
+    assert batch["input_ids"].shape == (4, 512)
+    assert batch["attention_mask"].shape == (4, 512)
+    assert batch["token_type_ids"].shape == (4, 512, 7)
     number_processed = 0
     total = len(train_dataloader) * batch["input_ids"].shape[0] # number of batches * batch_size
     print(f"Epoch: {ep}")
@@ -111,7 +114,7 @@ for batch in test_dataloader:
     # forward pass
     outputs = model(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, labels=labels)
     model_predictions = outputs.logits.argmax(-1)
-
+    print(model_predictions)
     # add metric
     accuracy.add_batch(predictions=model_predictions, references=labels)
 
